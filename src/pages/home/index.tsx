5096f097c1ed3a97445f4b2b6fa1ac58a4f5320b
Josué Alves <jow17alves@gmail.com>
2022-01-31 15:53:53 -0300

    feat: code preparation to receive an update from develop

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,18 +1,3 @@
-import { Box } from "@chakra-ui/react";
-import { Header } from "../../components/Header";
-import image from "../../assets/crianças_capstone_desktop.jpg";
-
 export const Home = () => {
-  return (
-    <Box
-      bgImage={image}
-      w="100vw"
-      h="100vh"
-      bgSize="cover"
-      backgroundPosition="center"
-    >
-      <Header />
-      Home
-    </Box>
-  );
+  return <div></div>;
 };
