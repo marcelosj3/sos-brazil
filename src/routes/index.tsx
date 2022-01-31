f73317b76112a2729ea8b769540953af3945356e
daviraquel <daviraquel@gmail.com>
2022-01-31 11:35:22 -0300

    feat: create login page

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -4,10 +4,12 @@ import { PrivateRoute } from "./PrivateRoute";
 
 import { Home } from "../pages/home";
 import { Register } from "../pages/register";
+import { Login } from "../pages/login";
 
 export const Routes = () => (
   <RoutesSwitch>
     <Route path="/" element={<Home />} />
     <Route path="/register" element={<Register />} />
+    <Route path="/login" element={<Login />} />
   </RoutesSwitch>
 );
